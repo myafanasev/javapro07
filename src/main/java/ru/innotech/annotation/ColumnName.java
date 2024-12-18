482176e544f08a47b957286e0536022d780ff1b9
Afanasev Mikhail <miafanasev@gmail.com>
2024-12-18 13:20:12 +0700

    Удалены неиспользуемые аннотации, DAO и DTO

--- src/main/java/ru/innotech/annotation/ColumnName.java
+++
@@ -1,8 +0,0 @@
-package ru.innotech.annotation;
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface ColumnName { // наименование столбца таблицы в БД
-    String value();
-}
--- src/main/java/ru/innotech/annotation/TableName.java
+++
@@ -1,9 +0,0 @@
-package ru.innotech.annotation;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface TableName {   // наименование таблицы в БД
-    String value();
-}
